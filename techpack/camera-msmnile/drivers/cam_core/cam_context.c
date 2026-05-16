UtsavBalar1231/kernel_xiaomi_sm8250

msm: camera: common: Clean shutdown and page fault sequence
Avoid unnecessary logging during shutdown and pagefault
in camera drivers.

CRs-Fixed: 2500721
Change-Id: I2519d45da134306f906186dc25705fc1f84b1972
Signed-off-by: Karthik Anantha Ram <kartanan@codeaurora.org>

drivers/cam_core/cam_context.c
@@ -54,12 +54,15 @@ int cam_context_shutdown(struct cam_context *ctx)
 		rc = -EINVAL;
 	}
 
-	rc = cam_destroy_device_hdl(ctx->dev_hdl);
-	if (rc)
-		CAM_ERR(CAM_CORE, "destroy device hdl failed for node %s",
-			ctx->dev_name);
-	else
-		ctx->dev_hdl = -1;
+	if (ctx->dev_hdl != -1) {
+		rc = cam_destroy_device_hdl(ctx->dev_hdl);
+		if (rc)
+			CAM_ERR(CAM_CORE,
+				"destroy device hdl failed for node %s",
+				ctx->dev_name);
+		else
+			ctx->dev_hdl = -1;
+	}
 
 	return rc;
 }
@@ -237,12 +240,15 @@ int cam_context_dump_pf_info(struct cam_context *ctx, unsigned long iova,
 		return -EINVAL;
 	}
 
-	if (ctx->state_machine[ctx->state].pagefault_ops) {
-		rc = ctx->state_machine[ctx->state].pagefault_ops(ctx, iova,
-			buf_info);
-	} else {
-		CAM_WARN(CAM_CORE, "No dump ctx in dev %d, state %d",
-			ctx->dev_hdl, ctx->state);
+	if ((ctx->state > CAM_CTX_AVAILABLE) &&
+		(ctx->state < CAM_CTX_STATE_MAX)) {
+		if (ctx->state_machine[ctx->state].pagefault_ops) {
+			rc = ctx->state_machine[ctx->state].pagefault_ops(
+				ctx, iova, buf_info);
+		} else {
+			CAM_WARN(CAM_CORE, "No dump ctx in dev %d, state %d",
+				ctx->dev_hdl, ctx->state);
+		}
 	}
 
 	return rc;

DUCKS-QCM-5-4/android_kernel_qcom_sm8150

techpack/camera-msmnile/drivers/cam_core/cam_context.c
@@ -61,12 +61,15 @@ int cam_context_shutdown(struct cam_context *ctx)
 		rc = -EINVAL;
 	}
 
-	rc = cam_destroy_device_hdl(ctx->dev_hdl);
-	if (rc)
-		CAM_ERR(CAM_CORE, "destroy device hdl failed for node %s",
-			ctx->dev_name);
-	else
-		ctx->dev_hdl = -1;
+	if (ctx->dev_hdl != -1) {
+		rc = cam_destroy_device_hdl(ctx->dev_hdl);
+		if (rc)
+			CAM_ERR(CAM_CORE,
+				"destroy device hdl failed for node %s",
+				ctx->dev_name);
+		else
+			ctx->dev_hdl = -1;
+	}
 
 	return rc;
 }
@@ -267,12 +270,15 @@ int cam_context_dump_pf_info(struct cam_context *ctx, unsigned long iova,
 	}
 
 	mutex_lock(&ctx->ctx_mutex);
-	if (ctx->state_machine[ctx->state].pagefault_ops) {
-		rc = ctx->state_machine[ctx->state].pagefault_ops(ctx, iova,
-			buf_info);
-	} else {
-		CAM_INFO(CAM_CORE, "No dump ctx in dev %d, state %d",
-			ctx->dev_hdl, ctx->state);
+	if ((ctx->state > CAM_CTX_AVAILABLE) &&
+		(ctx->state < CAM_CTX_STATE_MAX)) {
+		if (ctx->state_machine[ctx->state].pagefault_ops) {
+			rc = ctx->state_machine[ctx->state].pagefault_ops(
+				ctx, iova, buf_info);
+		} else {
+			CAM_INFO(CAM_CORE, "No dump ctx in dev %d, state %d",
+				ctx->dev_hdl, ctx->state);
+		}
 	}
 	mutex_unlock(&ctx->ctx_mutex);
 