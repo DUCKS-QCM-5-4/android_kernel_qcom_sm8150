MotorolaMobilityLLC/kernel-msm

fs: crypto: support IV_INO_LBLK_32 for legacy (V1) format
New file encryption V2 format suports IV_INO_LBLK_32 but this is
needed in V1 to support over the air upgrade which uses
FS_ENCRYPTION_MODE_PRIVATE data encryption mode. Also randomness of
encrypted data for eMMC devices is fixed.

Change-Id: Idb9f5a140e755a9f5c9aa26d5f0e900252f441e8
Signed-off-by: Neeraj Soni <neersoni@codeaurora.org>
Reviewed-on: https://gerrit.mot.com/1813341
SME-Granted: SME Approvals Granted
Tested-by: Jira Key
SLTApproved: Marcos Fukai Inoue <marcosi@motorola.com>
Submit-Approved: Marcos Fukai Inoue <marcosi@motorola.com>
Reviewed-by: Sergio Vilela <sv008@motorola.com>

fs/crypto/crypto.c
@@ -88,25 +88,31 @@ void fscrypt_generate_iv(union fscrypt_iv *iv, u64 lblk_num,
 #endif
 	memset(iv, 0, ci->ci_mode->ivsize);
 
-	if (flags & FSCRYPT_POLICY_FLAG_IV_INO_LBLK_64) {
-		WARN_ON_ONCE(lblk_num > U32_MAX);
-		WARN_ON_ONCE(ci->ci_inode->i_ino > U32_MAX);
-		lblk_num |= (u64)ci->ci_inode->i_ino << 32;
-	} else if (flags & FSCRYPT_POLICY_FLAG_IV_INO_LBLK_32) {
-		WARN_ON_ONCE(lblk_num > U32_MAX);
-		lblk_num = (u32)(ci->ci_hashed_ino + lblk_num);
-	} else if (flags & FSCRYPT_POLICY_FLAG_DIRECT_KEY) {
-		memcpy(iv->nonce, ci->ci_nonce, FS_KEY_DERIVATION_NONCE_SIZE);
-	} else if ((fscrypt_policy_contents_mode(&ci->ci_policy) ==
-						 FSCRYPT_MODE_PRIVATE)
-						 && inlinecrypt) {
-		if (ci->ci_inode->i_sb->s_type->name) {
-			if (!strcmp(ci->ci_inode->i_sb->s_type->name, "f2fs")) {
+	if ((fscrypt_policy_contents_mode(&ci->ci_policy) ==
+					  FSCRYPT_MODE_PRIVATE)
+					  && inlinecrypt) {
+		if (ci->ci_inode->i_sb->s_type->name &&
+		    !strcmp(ci->ci_inode->i_sb->s_type->name, "f2fs")) {
+			if (flags & FSCRYPT_POLICY_FLAG_IV_INO_LBLK_32) {
+				WARN_ON_ONCE(lblk_num > U32_MAX);
+				lblk_num = (u32)(ci->ci_hashed_ino + lblk_num);
+			} else {
 				WARN_ON_ONCE(lblk_num > U32_MAX);
 				WARN_ON_ONCE(ci->ci_inode->i_ino > U32_MAX);
 				lblk_num |= (u64)ci->ci_inode->i_ino << 32;
 			}
 		}
+	} else if (flags & FSCRYPT_POLICY_FLAG_IV_INO_LBLK_64) {
+		WARN_ON_ONCE(lblk_num > U32_MAX);
+		WARN_ON_ONCE(ci->ci_inode->i_ino > U32_MAX);
+		lblk_num |= (u64)ci->ci_inode->i_ino << 32;
+	} else if ((flags & FSCRYPT_POLICY_FLAG_IV_INO_LBLK_32) &&
+		   !(fscrypt_policy_contents_mode(&ci->ci_policy) ==
+		     FSCRYPT_MODE_PRIVATE)) {
+		WARN_ON_ONCE(lblk_num > U32_MAX);
+		lblk_num = (u32)(ci->ci_hashed_ino + lblk_num);
+	} else if (flags & FSCRYPT_POLICY_FLAG_DIRECT_KEY) {
+		memcpy(iv->nonce, ci->ci_nonce, FS_KEY_DERIVATION_NONCE_SIZE);
 	}
 	iv->lblk_num = cpu_to_le64(lblk_num);
 }

DUCKS-QCM-5-4/android_kernel_qcom_sm8150

Test: vts-kernel-encryption-test after Q to R OTA.
Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

fs/crypto/crypto.c
@@ -91,25 +91,31 @@ void fscrypt_generate_iv(union fscrypt_iv *iv, u64 lblk_num,
 
 	memset(iv, 0, ci->ci_mode->ivsize);
 
-	if (flags & FSCRYPT_POLICY_FLAG_IV_INO_LBLK_64) {
-		WARN_ON_ONCE(lblk_num > U32_MAX);
-		WARN_ON_ONCE(ci->ci_inode->i_ino > U32_MAX);
-		lblk_num |= (u64)ci->ci_inode->i_ino << 32;
-	} else if (flags & FSCRYPT_POLICY_FLAG_IV_INO_LBLK_32) {
-		WARN_ON_ONCE(lblk_num > U32_MAX);
-		lblk_num = (u32)(ci->ci_hashed_ino + lblk_num);
-	} else if (flags & FSCRYPT_POLICY_FLAG_DIRECT_KEY) {
-		memcpy(iv->nonce, ci->ci_nonce, FSCRYPT_FILE_NONCE_SIZE);
-	} else if ((fscrypt_policy_contents_mode(&ci->ci_policy) ==
-						 FSCRYPT_MODE_PRIVATE)
-						 && inlinecrypt) {
-		if (ci->ci_inode->i_sb->s_type->name) {
-			if (!strcmp(ci->ci_inode->i_sb->s_type->name, "f2fs")) {
+	if ((fscrypt_policy_contents_mode(&ci->ci_policy) ==
+					  FSCRYPT_MODE_PRIVATE)
+					  && inlinecrypt) {
+		if (ci->ci_inode->i_sb->s_type->name &&
+		    !strcmp(ci->ci_inode->i_sb->s_type->name, "f2fs")) {
+			if (flags & FSCRYPT_POLICY_FLAG_IV_INO_LBLK_32) {
+				WARN_ON_ONCE(lblk_num > U32_MAX);
+				lblk_num = (u32)(ci->ci_hashed_ino + lblk_num);
+			} else {
 				WARN_ON_ONCE(lblk_num > U32_MAX);
 				WARN_ON_ONCE(ci->ci_inode->i_ino > U32_MAX);
 				lblk_num |= (u64)ci->ci_inode->i_ino << 32;
 			}
 		}
+	} else if (flags & FSCRYPT_POLICY_FLAG_IV_INO_LBLK_64) {
+		WARN_ON_ONCE(lblk_num > U32_MAX);
+		WARN_ON_ONCE(ci->ci_inode->i_ino > U32_MAX);
+		lblk_num |= (u64)ci->ci_inode->i_ino << 32;
+	} else if ((flags & FSCRYPT_POLICY_FLAG_IV_INO_LBLK_32) &&
+		   !(fscrypt_policy_contents_mode(&ci->ci_policy) ==
+		     FSCRYPT_MODE_PRIVATE)) {
+		WARN_ON_ONCE(lblk_num > U32_MAX);
+		lblk_num = (u32)(ci->ci_hashed_ino + lblk_num);
+	} else if (flags & FSCRYPT_POLICY_FLAG_DIRECT_KEY) {
+		memcpy(iv->nonce, ci->ci_nonce, FSCRYPT_FILE_NONCE_SIZE);
 	}
 	iv->lblk_num = cpu_to_le64(lblk_num);
 }